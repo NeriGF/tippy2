72f2269ae874c6ec465332ec7fd7779442fa81d4
Neri <neri.garciafigueroa@gmail.com>
2019-12-17 20:27:19 -0800

    Initial Commit

---
+++ tippy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy2
+//
+//  Created by Neri Garcia on 12/17/19.
+//  Copyright © 2019 Neri Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
